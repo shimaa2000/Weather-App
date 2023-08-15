48c24600c16ba4b212e3f1229f35cb62d22ebee4
Shimaa <shimaam5256@gmail.com>
2023-08-15 17:51:57 +0300

    added service locator init to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/weather/presentation/screens/home_screen.dart';
 
+import 'core/services/services_locator.dart';
+
 void main() {
+  ServicesLocator().init();
   runApp(const  HomeScreen(),);
 }
