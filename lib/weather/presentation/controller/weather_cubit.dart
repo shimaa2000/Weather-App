80c7e66e41a1cf45342fb591a0561356d9a519f7
Shimaa <shimaam5256@gmail.com>
2023-08-15 18:01:57 +0300

    internet permission

--- lib/weather/presentation/controller/weather_cubit.dart
+++ lib/weather/presentation/controller/weather_cubit.dart
@@ -18,54 +18,9 @@ class WeatherCubit extends Cubit<WeatherState> {
   WeatherCubit(this._weatherByCountryName) : super(const WeatherState());
   final GetWeatherByCountryName _weatherByCountryName;
 
-  Future<bool> _handleLocationPermission() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      Fluttertoast.showToast(
-          msg: 'Location services are disabled. Please enable the services',
-          toastLength: Toast.LENGTH_SHORT,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      return false;
-    }
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        Fluttertoast.showToast(
-            msg: 'Location permission is denied',
-            toastLength: Toast.LENGTH_SHORT,
-            timeInSecForIosWeb: 1,
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0);
-        return false;
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      Fluttertoast.showToast(
-          msg:
-              'Location permissions are permanently denied, we cannot request permissions.',
-          toastLength: Toast.LENGTH_SHORT,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
-      return false;
-    }
-    return true;
-  }
-
 
 
   Future<Position?> _getCurrentPosition() async {
-    final hasPermission = await _handleLocationPermission();
-    if (!hasPermission) return null;
     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
         .then((Position position) {
       emit(state.copyWith(currentPosition: position));
